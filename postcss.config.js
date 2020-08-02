08e9f72172951345da8edc6d49b634ffc1c37552
Pruginkin <pruginkin@gmail.com>
2020-08-03 01:31:15 +0300

    fix unused css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,27 @@
+// const tailwindcss = require("tailwindcss");
+// const cssnano = require("cssnano");
+
+// module.exports = {
+//   plugins: [
+//     tailwindcss("./tailwind.config.js"),
+//     cssnano
+//   ],
+// };
+
+const cssnano = require("cssnano")({ preset: "default" });
 const tailwindcss = require("tailwindcss");
-const cssnano = require("cssnano");
+const autoprefixer = require('autoprefixer')({ overrideBrowserslist: ['last 2 versions'] });
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+    content: ["./src/**/*.svelte", "./src/**/*.html"],
+    keyframes: true,
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  plugins: [
-    tailwindcss("./tailwind.config.js"),
-    cssnano
-  ],
+    plugins: [
+        tailwindcss, ...(production ? [purgecss, autoprefixer, cssnano] : []),
+    ]
 };
